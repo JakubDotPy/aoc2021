50d3e59a8a1a39298df5e752653aa0369ccfc15b
Jakub Červinka <cervinka.jakub.1989@gmail.com>
2021-11-30 15:36:22 +0100

    Initial commit

---
+++ day00/part1.py
@@ -0,0 +1,42 @@
+import argparse
+import os.path
+
+import pytest
+
+from support.support import timing
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+# NOTE: paste test text here
+INPUT_S = """\
+"""
+
+
+def compute(s: str) -> int:
+    # TODO: implement solution here!
+    return 0
+
+
+@pytest.mark.template
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+            (INPUT_S, 0),
+            ),
+    )
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
